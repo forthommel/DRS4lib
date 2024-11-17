e237c8b6ca2f56b092ed2d389418338fdded8258
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-11-17 18:51:37 +0100

    Filled the read utility

--- src/read.cc
+++ src/read.cc
@@ -1,15 +1,23 @@
 #include <iostream>
 
 #include "DRS4lib/Calibrations.h"
+#include "DRS4lib/DataFormat.h"
 #include "DRS4lib/Reader.h"
 
 int main() {
   drs4::Calibrations calibrations("../data");
-  calibrations.addModuleCalibrations(13118, 2, 8, 1024);
+  const auto& module1_calibrations = calibrations.addModuleCalibrations(13118, 2, 8, 1024);
+  const auto& module2_calibrations = calibrations.addModuleCalibrations(21333, 2, 8, 1024);
 
   std::cout << calibrations << std::endl;
 
   drs4::Reader reader;
+  reader.addModule("input0.dat", 13118, module1_calibrations);
+  reader.addModule("input1.dat", 21333, module2_calibrations);
+
+  drs4::GlobalEvent global_event;
+  while (reader.next(global_event)) {
+  }
 
   return 0;
 }
